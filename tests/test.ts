a63a322ba1d762592af2e0e9b483c643d0515f9b
Ming-der Wang <mingderwang@gmail.com>
2022-07-23 00:05:36 +0800

    test more test

--- tests/test.ts
+++ tests/test.ts
@@ -1,13 +1,15 @@
 import test from "ava";
 
-import { sum, subtract } from "../dist/main";
+import { sum, subtract } from "../dist/main.js";
 
 test("sum", (t) => {
   t.is(sum(0, 0), 0);
   t.is(sum(2, 2), 4);
+  t.is(sum(2, 3), 5);
 });
 
 test("subtract", (t) => {
   t.is(subtract(0, 0), 0);
   t.is(subtract(4, 2), 2);
+  t.is(subtract(5, 2), 3);
 });
