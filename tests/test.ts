dab4ab6ce6073d65ff96b1f74b4dbdc9e5282efd
Ming-der Wang <mingderwang@gmail.com>
2022-07-23 00:08:07 +0800

    fix lint error

--- tests/test.ts
+++ tests/test.ts
@@ -1,14 +1,14 @@
-import test from "ava";
+import test from 'ava';
 
-import { sum, subtract } from "../dist/main.js";
+import {sum, subtract} from '../dist/main';
 
-test("sum", (t) => {
+test('sum', (t) => {
   t.is(sum(0, 0), 0);
   t.is(sum(2, 2), 4);
   t.is(sum(2, 3), 5);
 });
 
-test("subtract", (t) => {
+test('subtract', (t) => {
   t.is(subtract(0, 0), 0);
   t.is(subtract(4, 2), 2);
   t.is(subtract(5, 2), 3);
